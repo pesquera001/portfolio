f73a9e526e894b3cd9090f474074eb9f1bf476ee
Aidan Pesquera <aidanpesquera@Aidans-MacBook-Pro.local>
2025-07-23 17:10:01 -0700

    Replace cobalt blue with fancy white (#F5F5F7, #EDEDF0) in Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,8 +12,8 @@ module.exports = {
       colors: {
         'steel-gray': '#232a32',
         'light-steel-gray': '#232a32',
-        'cobalt-blue': '#2176FF',
-        'cobalt-blue-alt': '#1577D9',
+        'cobalt-blue': '#F5F5F7',
+        'cobalt-blue-alt': '#EDEDF0',
         'metallic-silver': '#B2B8BD',
         'light-gray': '#F3F4F6',
         'deep-navy-blue': '#181C24',
